75196d72431a0d87f503f134de83251dd9e003c1
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-15 11:05:55 +0000

    Add function for reading CD data files

--- process_cd_data.py
+++ process_cd_data.py
@@ -1,4 +1,5 @@
 import sys
+import re
 print("")
 
 # Get arguments
@@ -14,3 +15,26 @@ if len(sys.argv) <= 3:
     print("Where should the output .dat be saved?\n")
     sys.exit()
 output_path = sys.argv[3]
+
+# Define function for opening data file and getting relevant data
+def open_data_file(path):
+    with open(path) as f:
+        lines = [line.rstrip() for line in f.readlines() if line.rstrip()]
+    runs, run, in_data = [], [], False
+    for line in lines:
+        if in_data:
+            values = line.split()
+            try:
+                run.append({
+                 "wavelength": float(values[0]),
+                 "signal": float(values[1]),
+                 "error": float(values[2]),
+                })
+            except ValueError:
+                pass
+        if line == "$DATA": in_data = True
+        if line == "$ENDDATA": in_data = False
+        if line.startswith("$MDCNAME") and run:
+            runs.append(run)
+            run = []
+    runs.append(run)
