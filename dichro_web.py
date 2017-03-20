bf3050f55c1f1aca24563706df266322801fe0d5
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-20 13:23:43 +0000

    Dichroweb script can supply file

--- dichro_web.py
+++ dichro_web.py
@@ -1,4 +1,6 @@
-import selenium
+from selenium import webdriver
+from time import sleep
+import os
 import sys
 print("")
 
@@ -19,3 +21,38 @@ if len(sys.argv) < 5:
     print("What is the concentration in mg/ml?\n")
     sys.exit()
 concentration = float(sys.argv[4])
+
+# Get the files
+files = os.listdir(location)
+data_files = sorted([f for f in files if f[-4:] in (".gen", ".dat")])
+data_files = [os.path.abspath(location + "/" + f) for f in data_files]
+
+browser = webdriver.Chrome()
+try:
+    # Go to the input page
+    browser.get("http://dichroweb.cryst.bbk.ac.uk/html/process.shtml")
+    inputs = browser.find_elements_by_tag_name("input")
+
+    # Supply login details
+    username_input = [
+     i for i in inputs if i.get_attribute("name") == "CD-USERNAME"
+    ][0]
+    password_input = [
+     i for i in inputs if i.get_attribute("name") == "CD-PWD"
+    ][0]
+    username_input.send_keys(username)
+    password_input.send_keys(password)
+
+    # Supply data
+    name_input = [
+     i for i in inputs if i.get_attribute("name") == "NAME"
+    ][0]
+    data_file_input = [
+     i for i in inputs if i.get_attribute("name") == "FILE"
+    ][0]
+    name_input.send_keys(data_files[0].split("/")[-1])
+    data_file_input.send_keys(data_files[0])
+    sleep(5)
+
+finally:
+    browser.quit()
