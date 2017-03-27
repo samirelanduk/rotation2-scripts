97ef71b3f61c277f18b73e1e3dc476858b870fa0
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-27 17:55:27 +0100

    Add colour to charts

--- make_melt_chart.py
+++ make_melt_chart.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import matplotlib.pyplot as plt
+from omnicanvas import hsl_to_rgb
 print("")
 
 if len(sys.argv) <= 1:
@@ -21,12 +22,17 @@ data_files = sorted([f for f in os.listdir(data_path) if f[-4:] == ".gen"])
 
 # Open up the files and extract the delicious data
 all_series = []
+min_temp = start_temp
+max_temp = 90
 for index, data_file in enumerate(data_files):
     # What temperature is this?
     temp = start_temp + (index * temp_interval)
+    color = "#0000FF"
     if temp > 90:
         temp = "20°C (cooled)"
     else:
+        relative_temp = 1 - ((temp - min_temp) / (max_temp - min_temp))
+        color = hsl_to_rgb(0.45 * 360 * relative_temp, 100, 50)
         temp = "%i°C" % temp
 
     # Get the data series
@@ -34,13 +40,13 @@ for index, data_file in enumerate(data_files):
         lines = f.readlines()
     lines = [line.split() for line in lines]
     lines = [line for line in lines if line[0][:3].isdigit()]
-    series = [[float(line[0]), float(line[1]), temp] for line in lines]
+    series = [[float(line[0]), float(line[1]), temp, color] for line in lines]
     all_series.append(series)
 
 # Make chart of data
 for series in all_series:
     wavelengths = [line[0] for line in series]
     absorbance = [line[1] for line in series]
-    plt.plot(wavelengths, absorbance, label=series[0][2])
+    plt.plot(wavelengths, absorbance, color=series[0][3], label=series[0][2])
 plt.legend(prop={'size':7})
 plt.savefig(data_path + "/melt.png", dpi=500)
