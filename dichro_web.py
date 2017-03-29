5c9ae4d431dfb01ea40b0c916d90008cb7381294
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-29 18:08:28 +0100

    Dichroweb script can output JSON

--- dichro_web.py
+++ dichro_web.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from time import sleep
+from time import sleep, time
 from math import sqrt
+import json
 import inferi
 import os
 import sys
@@ -46,150 +47,164 @@ file_results = []
 for data_file in data_files[:3]:
     program_results = {"temperature": data_file["temperature"]}
     for program in ("SELCON3", "CONTIN", "CDSSTR"):
-        browser = webdriver.Chrome()
-        try:
-            # Go to the input page
-            browser.get("http://dichroweb.cryst.bbk.ac.uk/html/process.shtml")
-            inputs = browser.find_elements_by_tag_name("input")
-
-            # Supply login details
-            username_input = [
-             i for i in inputs if i.get_attribute("name") == "CD-USERNAME"
-            ][0]
-            password_input = [
-             i for i in inputs if i.get_attribute("name") == "CD-PWD"
-            ][0]
-            username_input.send_keys(username)
-            password_input.send_keys(password)
-
-            # Supply data
-            name_input = [
-             i for i in inputs if i.get_attribute("name") == "NAME"
-            ][0]
-            data_file_input = [
-             i for i in inputs if i.get_attribute("name") == "FILE"
-            ][0]
-            name_input.send_keys(data_file["location"].split("/")[-1].split(".")[0].replace("#", ""))
-            data_file_input.send_keys(data_file["location"])
-
-            # Describe data file
-            selects = browser.find_elements_by_tag_name("select")
-            file_format = [
-             s for s in selects if s.get_attribute("name") == "FORMAT"
-            ][0]
-            file_format = Select(file_format)
-            file_format.select_by_visible_text("FREE (with preview)")
-            units = [
-             s for s in selects if s.get_attribute("name") == "units"
-            ][0]
-            units = Select(units)
-            units.select_by_visible_text("millidegrees / theta (machine units)")
-            with open(data_files[0]["location"]) as f:
-                data = f.read()
-            lines = data.split("\n")
-            lines = [line.strip() for line in lines if line.strip()]
-            high_wavelength = 10000
-            low_wavelength = 0
-            for line in lines:
-                try:
-                    wavelength = float(line.split()[0])
-                    if high_wavelength == 10000:
-                        high_wavelength = wavelength
-                    low_wavelength = wavelength
-                except ValueError:
-                    pass
-            initial_wavelength = [
-             i for i in inputs if i.get_attribute("name") == "START"
-            ][0]
-            final_wavelength = [
-             i for i in inputs if i.get_attribute("name") == "END"
-            ][0]
-            initial_wavelength.send_keys(str(high_wavelength))
-            final_wavelength.send_keys(str(low_wavelength))
-            wavestep = [
-             i for i in inputs if i.get_attribute("name") == "WAVESTEP"
-            ][-1]
-            wavestep.click()
-            lowest_nm = [
-             i for i in inputs if i.get_attribute("name") == "QUALDATA"
-            ][0]
-            lowest_nm.send_keys(str(low_wavelength))
-
-            # Choose analysis methods
-            programs = [
-             s for s in selects if s.get_attribute("name") == "prog"
-            ][0]
-            programs = Select(programs)
-            programs.select_by_visible_text(program)
-            reference_sets = [
-             s for s in selects if s.get_attribute("name") == "basis"
-            ][0]
-            reference_sets = Select(reference_sets)
-            reference_sets.select_by_visible_text(
-             "SMP180 (Optimised for 190-240 nm # Less nm required)"
-            )
-
-            # Set output units and submit
-            output_units = [
-             s for s in selects if s.get_attribute("name") == "output_units"
-            ][0]
-            output_units = Select(output_units)
-            output_units.select_by_visible_text("theta (machine units), mdeg")
-            submit = [
-             i for i in inputs if i.get_attribute("name") == "submit data"
-            ][0]
-            submit.click()
-
-            # Provide mass, concentration, and pathlength
-            inputs = browser.find_elements_by_tag_name("input")
-            #84
-            mean_mass_input = [
-             i for i in inputs if i.get_attribute("name") == "MRW"
-            ][0]
-            concentration_input = [
-             i for i in inputs if i.get_attribute("name") == "CONC"
-            ][0]
-            pathlength_input = [
-             i for i in inputs if i.get_attribute("name") == "PATH"
-            ][0]
-            mean_mass_input.send_keys(str(residue_mass))
-            concentration_input.send_keys(str(concentration))
-            pathlength_input.send_keys(str(pathlength))
-            submit = [
-             i for i in inputs if i.get_attribute("value") == "submit"
-            ][0]
-            submit.click()
-
-            # Go to results page
-            inputs = browser.find_elements_by_tag_name("input")
-            submit = [
-             i for i in inputs if i.get_attribute("value") == "submit"
-            ][0]
-            submit.click()
-
-            # What are the results?
-            links = browser.find_elements_by_tag_name("a")
-            show_links = [a for a in links if a.text == "SHOW"]
+        scale_extra = 0
+        while program not in program_results:
+            browser = webdriver.Chrome()
             try:
-                show_links[0].click()
-                table = browser.find_elements_by_tag_name("table")[1]
-                values = [
-                 [td.text for td in row.find_elements_by_tag_name("td")[1:]]
-                for row in table.find_elements_by_tag_name("tr")[1:]]
-                helix_content = float(values[-1][0]) + float(values[-1][1])
-                program_results[program] = helix_content
-            except:
-                print("Fail: %s°C, %s" % (data_file["temperature"], program))
-        finally:
-            browser.quit()
+                # Go to the input page
+                browser.get("http://dichroweb.cryst.bbk.ac.uk/html/process.shtml")
+                inputs = browser.find_elements_by_tag_name("input")
+
+                # Supply login details
+                username_input = [
+                 i for i in inputs if i.get_attribute("name") == "CD-USERNAME"
+                ][0]
+                password_input = [
+                 i for i in inputs if i.get_attribute("name") == "CD-PWD"
+                ][0]
+                username_input.send_keys(username)
+                password_input.send_keys(password)
+
+                # Supply data
+                name_input = [
+                 i for i in inputs if i.get_attribute("name") == "NAME"
+                ][0]
+                data_file_input = [
+                 i for i in inputs if i.get_attribute("name") == "FILE"
+                ][0]
+                name_input.send_keys(data_file["location"].split("/")[-1].split(".")[0].replace("#", "") + "T" + str(int(time())))
+                data_file_input.send_keys(data_file["location"])
+
+                # Describe data file
+                selects = browser.find_elements_by_tag_name("select")
+                file_format = [
+                 s for s in selects if s.get_attribute("name") == "FORMAT"
+                ][0]
+                file_format = Select(file_format)
+                file_format.select_by_visible_text("FREE (with preview)")
+                units = [
+                 s for s in selects if s.get_attribute("name") == "units"
+                ][0]
+                units = Select(units)
+                units.select_by_visible_text("millidegrees / theta (machine units)")
+                with open(data_files[0]["location"]) as f:
+                    data = f.read()
+                lines = data.split("\n")
+                lines = [line.strip() for line in lines if line.strip()]
+                high_wavelength = 10000
+                low_wavelength = 0
+                for line in lines:
+                    try:
+                        wavelength = float(line.split()[0])
+                        if high_wavelength == 10000:
+                            high_wavelength = wavelength
+                        low_wavelength = wavelength
+                    except ValueError:
+                        pass
+                initial_wavelength = [
+                 i for i in inputs if i.get_attribute("name") == "START"
+                ][0]
+                final_wavelength = [
+                 i for i in inputs if i.get_attribute("name") == "END"
+                ][0]
+                initial_wavelength.send_keys(str(high_wavelength))
+                final_wavelength.send_keys(str(low_wavelength))
+                wavestep = [
+                 i for i in inputs if i.get_attribute("name") == "WAVESTEP"
+                ][-1]
+                wavestep.click()
+                lowest_nm = [
+                 i for i in inputs if i.get_attribute("name") == "QUALDATA"
+                ][0]
+                lowest_nm.send_keys(str(low_wavelength))
+
+                # Choose analysis methods
+                programs = [
+                 s for s in selects if s.get_attribute("name") == "prog"
+                ][0]
+                programs = Select(programs)
+                programs.select_by_visible_text(program)
+                reference_sets = [
+                 s for s in selects if s.get_attribute("name") == "basis"
+                ][0]
+                reference_sets = Select(reference_sets)
+                reference_sets.select_by_visible_text(
+                 "SMP180 (Optimised for 190-240 nm # Less nm required)"
+                )
+
+                # Set scale
+                scale_input = [
+                 i for i in inputs if i.get_attribute("name") == "scale_data"
+                ][0]
+                scale_value = 1 + scale_extra
+                scale_input.clear()
+                scale_input.send_keys(str(scale_value))
+                sleep(3)
+
+                # Set output units and submit
+                output_units = [
+                 s for s in selects if s.get_attribute("name") == "output_units"
+                ][0]
+                output_units = Select(output_units)
+                output_units.select_by_visible_text("theta (machine units), mdeg")
+                submit = [
+                 i for i in inputs if i.get_attribute("name") == "submit data"
+                ][0]
+                submit.click()
+
+                # Provide mass, concentration, and pathlength
+                inputs = browser.find_elements_by_tag_name("input")
+                #84
+                mean_mass_input = [
+                 i for i in inputs if i.get_attribute("name") == "MRW"
+                ][0]
+                concentration_input = [
+                 i for i in inputs if i.get_attribute("name") == "CONC"
+                ][0]
+                pathlength_input = [
+                 i for i in inputs if i.get_attribute("name") == "PATH"
+                ][0]
+                mean_mass_input.send_keys(str(residue_mass))
+                concentration_input.send_keys(str(concentration))
+                pathlength_input.send_keys(str(pathlength))
+                submit = [
+                 i for i in inputs if i.get_attribute("value") == "submit"
+                ][0]
+                submit.click()
+
+                # Go to results page
+                inputs = browser.find_elements_by_tag_name("input")
+                submit = [
+                 i for i in inputs if i.get_attribute("value") == "submit"
+                ][0]
+                submit.click()
+
+                # What are the results?
+                links = browser.find_elements_by_tag_name("a")
+                show_links = [a for a in links if a.text == "SHOW"]
+                try:
+                    show_links[0].click()
+                    table = browser.find_elements_by_tag_name("table")[1]
+                    values = [
+                     [td.text for td in row.find_elements_by_tag_name("td")[1:]]
+                    for row in table.find_elements_by_tag_name("tr")[1:]]
+                    helix_content = float(values[-1][0]) + float(values[-1][1])
+                    program_results[program] = helix_content
+                except:
+                    scale_extra += 0.0001
+                    print("Fail: %s°C, %s" % (data_file["temperature"], program))
+            finally:
+                browser.quit()
     values = [
-     program_results.get("SELCON"),
-     program_results.get("CONTIN"),
-     program_results.get("CDSSTR")
+     program_results["SELCON3"],
+     program_results["CONTIN"],
+     program_results["CDSSTR"]
     ]
     values = [v for v in values if v]
     values = inferi.Series(*values, sample=False)
     program_results["mean"] = values.mean()
     program_results["error"] = values.standard_deviation() / sqrt(len(values))
     file_results.append(program_results)
-print(file_results)
+
+with open("%s/ss.json" % location, "w") as f:
+    json.dump(file_results, f)
