9e71bedcafac1be249d13a8516ac4c7144cab378
Sam Ireland <sam.ireland.uk@gmail.com>
2017-04-12 15:16:32 +0100

    Make melt chart generator use smoothed series

--- make_melt_chart.py
+++ make_melt_chart.py
@@ -9,25 +9,53 @@ if len(sys.argv) <= 1:
     sys.exit()
 data_path = sys.argv[1]
 if len(sys.argv) <= 2:
-    print("Please provide the starting temperature\n")
+    print("Please provide a label in the form Agent,Sample\n")
     sys.exit()
-start_temp = float(sys.argv[2])
-if len(sys.argv) <= 3:
-    print("Please provide the temperature interval\n")
-    sys.exit()
-temp_interval = float(sys.argv[3])
-if len(sys.argv) <= 4:
-    print("Please provide a label in the form Agent,Run,Sample\n")
-    sys.exit()
-agent, run, sample = sys.argv[4].split(",")
+agent, sample = sys.argv[2].split(",")
 
 # What are the files here?
+data_files = sorted([f for f in os.listdir(data_path) if f[-4:] == ".gen"])
 
-data_files = sorted(
- [f for f in os.listdir(data_path) if f[-4:] == ".gen"],
- key=lambda k: int("".join([char for char in k.split()[-1] if char.isdigit()]))
-)
+# Open up the files and extract the delicious data
+all_series = []
+for data_file in data_files:
+    with open("%s/%s" % (data_path, data_file)) as f:
+        lines = f.readlines()
+    lines = [line.split() for line in lines]
+    lines = [line for line in lines if line[0][:3].isdigit()]
+    series = [[float(line[0]), float(line[3]), data_file.split(".")[0]] for line in lines]
+    all_series.append(series)
 
+# Make chart of data
+for series in all_series:
+    # What temperature is this?
+    label = series[0][2]
+    color = "#0000FF"
+    if "_" not in label:
+        temp = int(label)
+        relative_temp = 1 - ((temp - 20) / 70)
+        color = hsl_to_rgb(0.45 * 360 * relative_temp, 100, 50)
+
+    # Make label prettier
+    if "_" in label:
+        label = "20°C (cooled)"
+    else:
+        label += "°C"
+
+    wavelengths = [line[0] for line in series]
+    absorbance = [line[1] for line in series]
+    plt.plot(wavelengths, absorbance, color=color, label=label)
+
+plt.grid(True)
+plt.xlabel("Wavelength (nm)")
+plt.ylabel("Circular Dichroism")
+plt.title("%s: Sample %s" % (agent, sample))
+plt.xlim(all_series[0][-1][0], all_series[0][0][0])
+plt.ylim(-20, 40)
+plt.legend(prop={'size':7})
+plt.savefig(data_path + "/melt.png", dpi=500)
+
+'''
 # Open up the files and extract the delicious data
 all_series = []
 min_temp = start_temp
@@ -63,4 +91,4 @@ plt.title("%s: Run %s, Sample %s" % (agent, run, sample))
 plt.xlim(all_series[0][-1][0], all_series[0][0][0])
 plt.ylim(-20, 40)
 plt.legend(prop={'size':7})
-plt.savefig(data_path + "/melt.png", dpi=500)
+plt.savefig(data_path + "/melt.png", dpi=500)'''
